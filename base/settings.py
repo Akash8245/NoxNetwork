6ece3add6c0da6b6c67680e16370340725f610df
Akash <akash93433@gmail.com>
2024-06-21 20:16:07 +0530

    production ready v1.0.9

--- base/settings.py
+++ base/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
