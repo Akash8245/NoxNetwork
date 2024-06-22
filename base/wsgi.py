9c2dabb3e50736b580301c728708ebb65b1c67b7
Akash <akash93433@gmail.com>
2024-06-22 15:34:41 +0530

    v2.0.0 --Production Deployment (changed DB to SQLite )

--- base/wsgi.py
+++ base/wsgi.py
@@ -5,5 +5,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 
 application = get_wsgi_application()
-
-app = get_wsgi_application()
